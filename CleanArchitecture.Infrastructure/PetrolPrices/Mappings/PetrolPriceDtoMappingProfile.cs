b84b163b2f9807e87a64eb77a3ae35c5eeacb18c
kodehue <hubertgraham@outlook.com>
2022-02-03 07:52:51 -0500

    updated mapping for petrol pricing

--- CleanArchitecture.Infrastructure/PetrolPrices/Mappings/PetrolPriceDtoMappingProfile.cs
+++ CleanArchitecture.Infrastructure/PetrolPrices/Mappings/PetrolPriceDtoMappingProfile.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using CleanArchitecture.Application.PetrolPrices.DTOs;
-using Joa = JamaicaOpenData.SharedLibrary.PetrolPrices.DTOs;
+using CleanArchitecture.SharedLibrary.Common.DTOs;
+using JoaCommon = JamaicaOpenData.SharedLibrary.Common.DTOs;
+using JoaPetrol = JamaicaOpenData.SharedLibrary.PetrolPrices.DTOs;
 
 namespace CleanArchitecture.Infrastructure.PetrolPrices.Mappings
 {
@@ -9,10 +11,19 @@ namespace CleanArchitecture.Infrastructure.PetrolPrices.Mappings
 
         public PetrolPriceDtoMappingProfile()
         {
-            CreateMap<Joa.PetrolPriceDto, OutletDto>()
+            CreateMap<JoaPetrol.PetrolPriceDto, OutletDto>()
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.OutletId))
                 .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.OutletName));
-            CreateMap<Joa.PetrolPriceDto, PetrolPriceDto>(); 
+            CreateMap<JoaPetrol.PetrolPriceDto, PetrolPriceDto>()
+                .ForMember(dest => dest.Outlet, opt => opt.MapFrom(src => src));
+
+            CreateMap<JoaCommon.JoaResultDto<JoaPetrol.PetrolPriceDto>, PaginatedList<PetrolPriceDto>>()
+            .ForMember(dest => dest.Items, opt => opt.MapFrom(src => src.Records))
+            .ForMember(dest => dest.TotalCount, opt => opt.MapFrom(src => src.Total))
+            .ForMember(dest => dest.ItemsPerPage, opt => opt.MapFrom(src => src.Limit))
+            .ForMember(dest => dest.TotalPages, opt => opt.MapFrom(src => src.Total / src.Limit))
+            .ForMember(dest => dest.CurrentPageNumber, opt => opt.Ignore());
+
         }
     }
 }
