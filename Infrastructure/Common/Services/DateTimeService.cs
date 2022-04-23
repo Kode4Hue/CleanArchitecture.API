a59cf84e82c2446eb591ce43d61989dbee4285b8
kodehue <hubertgraham@outlook.com>
2022-04-23 07:31:40 -0500

    adding auditing and datetime service

---
+++ Infrastructure/Common/Services/DateTimeService.cs
@@ -0,0 +1,12 @@
+﻿using NodaTime;
+
+namespace Infrastructure.PetrolPrices.Services
+{
+    public class DateTimeService
+    {
+        public ZonedDateTime GetZonedDateTime(Instant instant, string timezone)
+        {
+            return instant.InZone(DateTimeZoneProviders.Tzdb[timezone]);
+        }
+    }
+}
